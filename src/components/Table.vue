022fe758a22b87456e6068fff790de02c27eed56
nakario <nakario.jp@gmail.com>
2020-10-31 01:32:57 +0900

    Show only best combinations

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -5,13 +5,8 @@
         <h2 class="headline font-weight-bold mb-3">
           ロングブラスターネクロ サブ2回使用後メイン回数
         </h2>
-        <v-data-table
-          :headers="headers"
-          :items="values"
-          :sort-by="['repeatFloor', 'sum', 'repeat']"
-          :sort-desc="[true, false, true]"
-          multi-sort
-        ></v-data-table>
+        <v-switch v-model="showAll" label="全件表示"></v-switch>
+        <v-data-table :headers="headers" :items="filtered"></v-data-table>
       </v-col>
     </v-row>
   </v-container>
@@ -22,10 +17,20 @@ import Vue from "vue";
 import { mainInkUsePercentage, subInkUsePercentage } from "@/system";
 import { combinations } from "@/combinations";
 
+interface ColumnItem {
+  main: number;
+  sub: number;
+  sum: number;
+  repeat: number;
+  repeatFloor: number;
+  best: boolean;
+}
+
 export default Vue.extend({
   name: "Table",
 
   data: () => ({
+    showAll: false,
     headers: [
       {
         text: "メイン効率",
@@ -49,22 +54,50 @@ export default Vue.extend({
       }
     ],
     values: (() => {
-      const vs = [];
+      const vs: ColumnItem[] = [];
       for (const p of combinations()) {
         let ink = 100;
         const subUsed = subInkUsePercentage(40, p.sub, 4) * 2;
         ink -= subUsed;
         const repeat = ink / mainInkUsePercentage(11, p.main, 2);
+        const repeatFloor = Math.floor(repeat);
         vs.push({
           main: p.main,
           sub: p.sub,
           sum: p.main + p.sub,
           repeat: repeat,
-          repeatFloor: Math.floor(repeat)
+          repeatFloor: repeatFloor,
+          best: false
         });
+      }
+      vs.sort((a: ColumnItem, b: ColumnItem): number => {
+        if (a.repeatFloor > b.repeatFloor) return -1;
+        if (a.repeatFloor < b.repeatFloor) return 1;
+        if (a.sum < b.sum) return -1;
+        if (a.sum > b.sum) return 1;
+        if (a.repeat > b.repeat) return -1;
+        if (a.repeat < b.repeat) return 1;
+        if (a.main < b.main) return -1;
+        if (a.main > b.main) return 1;
+        if (a.sub < b.sub) return -1;
+        if (a.sub > b.sub) return 1;
+        return 0;
+      });
+      let best = undefined;
+      for (let i = 0; i < vs.length; i++) {
+        if (best != vs[i].repeatFloor) {
+          console.log(i, best, vs[i].repeatFloor);
+          best = vs[i].repeatFloor;
+          vs[i].best = true;
+        }
       }
       return vs;
     })()
-  })
+  }),
+  computed: {
+    filtered: function() {
+      return this.showAll ? this.values : this.values.filter(v => v.best);
+    }
+  }
 });
 </script>
