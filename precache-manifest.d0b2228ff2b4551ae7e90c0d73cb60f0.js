11f53504b1d728d355c476adfa71155ced5ec28d
nakario <nakario.jp@gmail.com>
2022-05-04 17:36:10 +0900

    deploy

---
+++ precache-manifest.d0b2228ff2b4551ae7e90c0d73cb60f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06d2752526b42a6e0f8e",
+    "url": "/splatoon-best-gear/css/chunk-vendors.0a178a38.css"
+  },
+  {
+    "revision": "30b2f2fe990068145864c3408b759081",
+    "url": "/splatoon-best-gear/img/ogp-image.png"
+  },
+  {
+    "revision": "626c986bc4405bc4f6f42e675091d608",
+    "url": "/splatoon-best-gear/index.html"
+  },
+  {
+    "revision": "5ef522e2cc3fc2fbdaf7",
+    "url": "/splatoon-best-gear/js/app.f8b94b65.js"
+  },
+  {
+    "revision": "06d2752526b42a6e0f8e",
+    "url": "/splatoon-best-gear/js/chunk-vendors.61b55a6f.js"
+  },
+  {
+    "revision": "a10480df9cf577eb8b5271f63be52536",
+    "url": "/splatoon-best-gear/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/splatoon-best-gear/robots.txt"
+  }
+]);
